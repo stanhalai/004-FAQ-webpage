e0d440fd9376aa9c6e4865714c28ffaad9c9154e
StanV <stan.halai@gmail.com>
2021-09-27 23:45:33 -0500

    some js fixes

--- app/js/index.js
+++ app/js/index.js
@@ -2,14 +2,18 @@ const faq = document.querySelector('.faq__block');
 const faqItems = document.querySelectorAll('.faq__item');
 const faqQs = document.querySelectorAll('.faq__question');
 
-console.log(faqItems);
-// faq.addEventListener ('click', e => {
-//     console.log('Clicked');
-// })
 
 faqQs.forEach((item, index) => {
     item.addEventListener('click', e => {
-        faqItems[index].classList.toggle('active')
+        clearActiveItems(index)
+        faqItems[index].classList.toggle('active')      
     } )
 })
 
+const clearActiveItems = selectedIndex => {
+    faqQs.forEach((item, index) => {
+        if (selectedIndex !== index) {
+            faqItems[index].classList.remove('active') 
+        }      
+    })
+  }
